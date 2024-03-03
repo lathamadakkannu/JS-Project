fcff30d859db870ba7dfd250971beca51ea69b54
lathamadakkannu <lathamadakkannu@gmail.com>
2024-03-03 18:37:00 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,517 @@
+var food=[
+    {
+        id:1,
+        name:"Pizza Hut",
+        rating:4.2,
+        time:"30-35 MINS",
+        type:"Pizzas",
+        place:"Tirunelveli",
+        price:220,
+        offer:"20% Offer",
+        offerConfirm:"yes",
+        image:"image/img1.jfif",
+        food:"non-veg",
+    },
+
+    {
+        id:2,
+        name:"Hotel Nellai Saravana Bhava",
+        rating:4.4,
+        time:"15-20 MINS",
+        type:"Desserts, Chinese",
+        place:"Tirunelveli",
+        price:100,
+        offer:"10% Offer",
+        offerConfirm:"yes",
+        image:"image/img2.jfif",
+        food:"veg",
+    },
+
+    {
+        id:3,
+        name:"Annapoorna Garden",
+        rating:4.5,
+        time:"30-35 MINS",
+        type:"South Indian, Chinese",
+        place:"Melapalayam",
+        price:100,
+        offer:"15% Offer",
+        offerConfirm:"yes",
+        image:"image/img3.jfif",
+        food:"veg",
+    },
+
+    {
+        id:4,
+        name:"Hotel Raja",
+        rating:4.4,
+        time:"20-25 MINS",
+        type:"Chinese, North Indian",
+        place:"Vannarpettai",
+        price:230,
+        offer:"no Offer",
+        offerConfirm:"no",
+        image:"image/img4.jfif",
+        food:"non-veg",
+    },
+
+    {
+        id:5,
+        name:"LunchBox - Meals And Thalis",
+        rating:4.3,
+        time:"30-35mins",
+        type:"Biryani, North Indian",
+        place:"Tirunelveli",
+        price:145,
+        offer:"25% Offer",
+        offerConfirm:"yes",
+        image:"image/img5.jfif",
+        food:"non-veg",
+    },
+
+    {
+        id:6,
+        name:"Faasos -Rolls & Shawarma",
+        rating:4.3,
+        time:"30-35mins",
+        type:"Kebabs, Fast Food",
+        place:"Tirunelveli",
+        price:125,
+        offer:"no Offer",
+        offerConfirm:"no",
+        image:"image/img6.jfif",
+        food:"veg",
+    },
+
+    {
+        id:7,
+        name:"KFC",
+        rating:4.3,
+        time:"20-25mins",
+        type:"Burgers, Biryani",
+        place:"Tirunelveli",
+        price:350,
+        offer:"15% Offer",
+        offerConfirm:"yes",
+        image:"image/img7.jfif",
+        food:"non-veg",
+    },
+
+    {
+        id:8,
+        name:"The Good Bowl",
+        rating:4.2,
+        time:"30-35mins",
+        type:"Biryani, North Indian",
+        place:"Tirunelveli",
+        price:250,
+        offer:"no Offer",
+        offerConfirm:"no",
+        image:"image/img8.jfif",
+        food:"non-veg",
+    },
+
+    {
+        id:9,
+        name:"Ubaids Nsk Friends Biriyani",
+        rating:4.2,
+        time:"30-35mins",
+        type:"Biryani, Chinese",
+        place:"Palayamkottai",
+        price:180,
+        offer:"15% Offer",
+        offerConfirm:"yes",
+        image:"image/img9.jfif",
+        food:"non-veg",
+    },
+
+    {
+        id:10,
+        name:"Vairamaligai Takeaway",
+        rating:4.4,
+        time:"30-35mins",
+        type:"Chinese, North Indian",
+        place:"Ktc Nagar",
+        price:155,
+        offer:"no Offer",
+        offerConfirm:"no",
+        image:"image/img10.jfif",
+        food:"non-veg",
+    },
+
+    {
+        id:11,
+        name:"Arabian Hot Grill",
+        rating:4.2,
+        time:"30-35mins",
+        type:"Biryani, Arabian",
+        place:"Palayamkottai",
+        price:140,
+        offer:"20% Offer",
+        offerConfirm:"yes",
+        image:"image/img11.jfif",
+        food:"non-veg",
+    },
+
+    {
+        id:12,
+        name:"Kasali Hotel",
+        rating:4.2,
+        time:"20-25mins",
+        type:"Biryani, Indian",
+        place:"North bye pass road",
+        price:90,
+        offer:"no Offer",
+        offerConfirm:"no",
+        image:"image/img12.jfif",
+        food:"veg",
+    },
+
+    {
+        id:13,
+        name:"Hotel Afna Park",
+        rating:4.3,
+        time:"25-30mins",
+        type:"Chinese, South Indian",
+        place:"Vannarpettai",
+        price:175,
+        offer:"20% Offer",
+        offerConfirm:"yes",
+        image:"image/img13.jfif",
+        food:"veg",
+    },
+
+
+    {
+        id:14,
+        name:"Shah Bucket Biryani",
+        rating:4.2,
+        time:"30-35mins",
+        type:"Biryani",
+        place:"Palayamkottai",
+        price:130,
+        offer:"no Offer",
+        offerConfirm:"no",
+        image:"image/img14.jfif",
+        food:"non-veg",
+    },
+
+
+    {
+        id:15,
+        name:"Hotel Banu Birunthavan",
+        rating:4.3,
+        time:"30-35mins",
+        type:"South Indian, Chinese",
+        place:"Palayamkottai",
+        price:270,
+        offer:"no Offer",
+        offerConfirm:"no",
+        image:"image/img15.jfif",
+        food:"non-veg",
+    },
+
+
+    {
+        id:16,
+        name:"Sithi Kitchen",
+        rating:4.2,
+        time:"30-35mins",
+        type:"Biryani, Chinese",
+        place:"Perumalpuram",
+        price:135,
+        offer:"15% Offer",
+        offerConfirm:"yes",
+        image:"image/img16.jfif",
+        food:"non-veg",
+    },
+
+    {
+        id:17,
+        name:"Blaack Forest",
+        rating:4.6,
+        time:"25-30mins",
+        type:"Desserts, Bakery",
+        place:"Tirunelveli",
+        price:200,
+        offer:"no Offer",
+        offerConfirm:"no",
+        image:"image/img17.jfif",
+        food:"sweets",
+    },
+
+
+    {
+        id:18,
+        name:"Baskin Robbins - Ice Cream",
+        rating:4.5,
+        time:"20-25mins",
+        type:"Desserts, Ice Cream",
+        place:"Tirunelveli",
+        price:100,
+        offer:"10% Offer",
+        offerConfirm:"yes",
+        image:"image/img18.jfif",
+        food:"sweets",
+    },
+
+    {
+        id:19,
+        name:"Cream & Fudge",
+        rating:3.5,
+        time:"15-20mins",
+        type:"Ice Cream, Beverages",
+        place:"Tirunelveli",
+        price:80,
+        offer:"no Offer",
+        offerConfirm:"no",
+        image:"image/img19.jfif",
+        food:"sweets",
+    },
+    {
+        id:20,
+        name:"Gourmet Ice Cream & Cakes",
+        rating:4.9,
+        time:"20-25mins",
+        type:"Desserts, Ice Cream",
+        place:"Tirunelveli",
+        price:120,
+        offer:"no Offer",
+        offerConfirm:"no",
+        image:"image/img20.jfif",
+        food:"sweets",
+    }
+]
+
+
+
+
+var foodType_Row=document.getElementById("card-row")
+
+
+foodfilter(food)
+
+function foodfilter(a){
+a.forEach(function(e){
+
+
+var foodType=document.createElement("div")
+foodType.classList.add("food-card")
+foodType_Row.appendChild(foodType)
+
+
+var foodType_img=document.createElement("div")
+foodType_img.classList.add("food-img")
+foodType.appendChild(foodType_img)
+
+var food_image=document.createElement("img")
+food_image.src=e.image
+foodType_img.appendChild(food_image)
+
+var foodType_header=document.createElement("div")
+foodType_header.classList.add("food-header")
+foodType.appendChild(foodType_header)
+foodType_header.innerHTML=e.name
+
+var foodType_offer=document.createElement("div")
+foodType_offer.classList.add("food-offer")
+foodType.appendChild(foodType_offer)
+foodType_offer.innerHTML=e.offer
+
+if(e.offer=="no Offer"){
+        foodType_offer.remove()
+}
+
+var foodType_type=document.createElement("div")
+foodType_type.classList.add("food-type")
+foodType.appendChild(foodType_type)
+
+var foodType_icon=document.createElement("i")
+foodType_icon.classList.add("fa", "fa-star")
+foodType_icon.setAttribute("aria-hidden","true")
+foodType_type.appendChild(foodType_icon)
+foodType_icon.innerHTML=" "+e.rating
+
+
+var foodType_para=document.createElement("span")
+foodType_para.classList.add("food-type")
+foodType_type.appendChild(foodType_para)
+foodType_para.innerHTML=" "+e.type
+
+foodType_icon.style.color="#4F7942"
+
+var foodType_price=document.createElement("div")
+foodType_price.classList.add("food-price")
+foodType.appendChild(foodType_price)
+foodType_price.innerHTML="Rs. "+e.price
+
+var foodType_place=document.createElement("div")
+foodType_place.classList.add("food-place")
+foodType.appendChild(foodType_place)
+foodType_place.innerHTML=e.place
+
+})
+}
+
+
+
+
+function sort(){
+
+var card=document.querySelectorAll(".food-card")
+console.log(card)
+
+card.forEach(function(e){
+    e.remove()
+})
+
+var so=food.slice().sort(function(a,b){
+    return b.price-a.price
+})
+
+foodfilter(so)
+
+}
+
+function rating(){
+
+var card=document.querySelectorAll(".food-card")
+console.log(card)
+
+card.forEach(function(e){
+    e.remove()
+})
+
+var rate=food.filter(function(rate){
+   return rate.rating>=4.4
+})
+
+foodfilter(rate)
+
+}
+
+function veg(){
+
+var card=document.querySelectorAll(".food-card")
+console.log(card)
+
+card.forEach(function(e){
+    e.remove()
+})
+
+var types=food.filter(function(pure){
+   return pure.food=="veg"
+})
+
+foodfilter(types)
+
+}
+
+function Nonveg(){
+
+var card=document.querySelectorAll(".food-card")
+console.log(card)
+
+card.forEach(function(e){
+    e.remove()
+})
+
+var Nveg=food.filter(function(no){
+    return no.food=="non-veg"
+})
+
+foodfilter(Nveg)
+
+}
+
+
+function offer(){
+
+var card=document.querySelectorAll(".food-card")
+console.log(card)
+
+card.forEach(function(e){
+    e.remove()
+})
+
+var off=food.filter(function(a){
+    return a.offerConfirm=="yes"
+})
+
+foodfilter(off)
+
+}
+
+
+function amount(){
+
+var card=document.querySelectorAll(".food-card")
+console.log(card)
+
+card.forEach(function(e){
+    e.remove()
+})
+
+var abo=food.filter(function(a){
+    return a.price>120 && a.price<=200
+})
+
+foodfilter(abo)
+
+}
+
+    
+function dessert(){
+
+var card=document.querySelectorAll(".food-card")
+console.log(card)
+
+card.forEach(function(e){
+    e.remove()
+})
+
+var sweet=food.filter(function(s){
+    return s.food=="sweets"
+})
+
+foodfilter(sweet)
+
+}
+
+
+function order(){
+
+    var card=document.querySelectorAll(".food-card")
+    console.log(card)
+    
+    card.forEach(function(e){
+        e.remove()
+    })
+    
+    var deliver=food.filter(function(d){
+        return d.time=="20-25mins"
+    })
+    
+    foodfilter(deliver)
+    
+    }
+
+
+
+function relevent(){
+
+var card=document.querySelectorAll(".food-card")
+console.log(card)
+
+card.forEach(function(e){
+    e.remove()
+})
+
+foodfilter(food)
+
+}
+
+
+
+
+
